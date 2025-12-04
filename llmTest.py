8c5fbdd90ff5b779e996a08dad618abd3d36f0b3
Akhilesh Vijay <43087665+akhileshvj@users.noreply.github.com>
2025-12-04 23:13:59 +0530

    Modify ChatOpenAI configuration in llmTest.py
    Updated API key and base URL for ChatOpenAI client.

--- llmTest.py
+++ llmTest.py
@@ -3,9 +3,10 @@ import os
 import httpx 
 client = httpx.Client(verify=False)
 llm = ChatOpenAI(
-    base_url="https://genailab.tcs.in",
+    base_url="url",
     model="azure_ai/genailab-maas-DeepSeek-V3-0324",
-    api_key="sk-PHXT5d_JGKnDksrzSEI0Xg", # This key is for Hackathon purposes only and should not be used for any unauthorized purposes
+    api_key="abc", # This key is for Hackathon purposes only and should not be used for any unauthorized purposes
     http_client=client
 )
+
 print(llm.invoke("Hi"))
